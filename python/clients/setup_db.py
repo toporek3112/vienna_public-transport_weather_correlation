53b76ab1feb103fdbae9f11c029e0bb61379b324
Karol Topor <toporkarol@gmail.com>
2025-04-09 12:08:09 +0200

    updated setup_db and database dump

--- python/clients/setup_db.py
+++ python/clients/setup_db.py
@@ -9,13 +9,6 @@ from utils.database import Database
 class SetupDB():
   def __init__(self):
     self.logger = logging.getLogger(__name__)
-    self.stops_url = 'https://www.wienerlinien.at/ogd_realtime/doku/ogd/gtfs/stops.txt'
-    self.stops_file = 'data/stops.txt'
-    self.db_table_stops = 'stops'
-
-    self.shapes_url = 'https://www.wienerlinien.at/ogd_realtime/doku/ogd/gtfs/shapes.txt'
-    self.shapes_file = 'data/shapes.txt'
-    self.db_table_shapes = 'shapes'
 
     # Get the engine from the Database singleton
     self.engine = Database.get_engine()
@@ -34,61 +27,16 @@ class SetupDB():
     WeatherData.metadata.create_all(self.engine)
     ProducerDelaysCheckpoint.metadata.create_all(self.engine)
     ProducerWeatherCheckpoint.metadata.create_all(self.engine)
-    self._load_stops()
-    self._load_shapes()
 
-  def _load_stops(self):
-    self.logger.info('***** Setting up stops table in database *****')
-    try:
-      # Try reading from URL
-      df_stops = pd.read_csv(self.stops_url)
-    except Exception:
-      # Fallback: read from file
-      df_stops = pd.read_csv(self.stops_file)
+    routes = pd.read_csv('data/routes.txt')
+    routes.to_sql("routes",self.engine, if_exists='replace', index=False)
+    trips = pd.read_csv('data/trips.txt')
+    trips.to_sql("trips",self.engine, if_exists='replace', index=False)
+    stop_times = pd.read_csv('data/stop_times.txt')
+    stop_times.to_sql("stops_times",self.engine, if_exists='replace', index=False)
+    stop_shapes = pd.read_csv('data/shapes.txt')
+    stop_shapes.to_sql("shapes",self.engine, if_exists='replace', index=False)
+    stop_stops = pd.read_csv('data/stops.txt')
+    stop_stops.to_sql("stops",self.engine, if_exists='replace', index=False)
 
-    df_stops.columns = ["id", "name", "latitude", "longitude", "zone_id"]
-    df_stops.to_sql(self.db_table_stops, self.engine, if_exists='replace', index=False)
-    self.logger.info('Data Inserted into Table')
-
-    # Query to select the first 10 rows from the table
-    query = f"SELECT * FROM {self.db_table_stops} LIMIT 10"
-    result = pd.read_sql(query, self.engine)
-    self.logger.info(f"First 10 rows from the '{self.db_table_stops}' table:")
-    self.logger.info(result)
-
-  def _load_shapes(self):
-    self.logger.info('***** Setting up shapes table in database *****')
-    try:
-      # Try reading from URL
-      df_shapes = pd.read_csv(self.shapes_url)
-    except Exception:
-      # Fallback: read from file
-      df_shapes = pd.read_csv(self.shapes_file)
-
-    # rename columns 
-    df_shapes.columns = ["id", "latitude", "longitude", "sequence", "shape_dist_traveled"]
-
-    # Filter for lines with 'H' in the ID indicating the line direction
-    # H = Hin, R = Retour (just an assumption)
-    df_shapes_filtered = df_shapes[df_shapes['id'].str.contains(r'.*H', regex=True)].copy()
-
-    # Extract version and line from the ID
-    df_shapes_filtered['version'] = df_shapes_filtered['id'].str.extract(r'-j25-(.*)\.H').astype(float)
-    df_shapes_filtered['line'] = df_shapes_filtered['id'].str.extract(r'^[^-]+-([^-]+)-')
-
-    # Get row count per id, version, and line
-    rows_count = df_shapes_filtered.groupby(['id', 'version', 'line']).size().reset_index(name='count')
-    df_shapes_clean = rows_count.sort_values(
-                                by=['line', 'count', 'version'],
-                                ascending=[True, False, False]
-                              ).groupby('line').head(1)
-
-    # Save to DB
-    df_shapes_filtered[df_shapes_filtered['id'].isin(df_shapes_clean['id'])].to_sql(self.db_table_shapes, self.engine, if_exists='replace', index=False)
-    self.logger.info('Data Inserted into Table')
-
-    # Show preview
-    query = f"SELECT * FROM {self.db_table_shapes} LIMIT 10"
-    result = pd.read_sql(query, self.engine)
-    self.logger.info(f"First 10 rows from the '{self.db_table_shapes}' table:")
-    self.logger.info(f"\n{result}")
+    self.logger.info('***** Tables created *****')
