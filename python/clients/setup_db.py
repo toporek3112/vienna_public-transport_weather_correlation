a1eb27707c6bc387b7efdc17e24d004c0590a949
Topor Karol <karol.topor@a1.at>
2025-04-09 13:25:42 +0200

    working

--- python/clients/setup_db.py
+++ python/clients/setup_db.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import logging
+import gzip
+import shutil
+import os
+import subprocess
 from clients.events.delay import Delay
 from clients.events.weather_data import WeatherData
 from utils.producer_delays_checkpoint import ProducerDelaysCheckpoint
@@ -28,6 +32,42 @@ class SetupDB():
     ProducerDelaysCheckpoint.metadata.create_all(self.engine)
     ProducerWeatherCheckpoint.metadata.create_all(self.engine)
 
+    self.load_db_from_gz()
+
+    self.logger.info('***** Tables created *****')
+
+  def load_db_from_gz(self):
+    # Unzip the SQL file
+    here = os.path.dirname(os.path.abspath(__file__))  # points to clients/
+    gz_file = os.path.join(here, '..', 'data', 'db.sql.gz')
+    sql_file = os.path.join(here, '..', 'data', 'db.sql')
+
+    self.logger.info("Unzipping SQL file...")
+    with gzip.open(gz_file, 'rb') as f_in, open(sql_file, 'wb') as f_out:
+      shutil.copyfileobj(f_in, f_out)
+
+    # Step 2: Restore via psql
+    # Adjust the connection parameters below (host, user, dbname, etc.)
+    self.logger.info("Running psql to restore the database dump...")
+    env = os.environ.copy()
+    env["PGPASSWORD"] = "postgres"
+    subprocess.run(
+      [
+        'psql',
+        '-h', 'vptwc_postgres',
+        '-p', '5432',
+        '-U', 'postgres',
+        '-d', 'vptwc_project',
+        '-f', sql_file
+      ],
+      check=True,
+      env=env
+    )
+    self.logger.info("Database dump restored successfully.")
+
+    self.logger.info("SQL file loaded successfully.")
+
+  def __initial_wl(self):
     routes = pd.read_csv('data/routes.txt')
     routes.to_sql("routes",self.engine, if_exists='replace', index=False)
     trips = pd.read_csv('data/trips.txt')
@@ -38,5 +78,3 @@ class SetupDB():
     stop_shapes.to_sql("shapes",self.engine, if_exists='replace', index=False)
     stop_stops = pd.read_csv('data/stops.txt')
     stop_stops.to_sql("stops",self.engine, if_exists='replace', index=False)
-
-    self.logger.info('***** Tables created *****')
